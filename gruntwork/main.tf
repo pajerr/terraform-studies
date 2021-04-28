1eea0e4c5cf6248fa59d933eb71b053b773c2bf4
Erno Paju <erno@Ernos-MacBook-Air.local>
2021-04-28 21:29:24 +0300

    päivitetty

--- gruntwork/main.tf
+++ gruntwork/main.tf
@@ -2,15 +2,19 @@ provider "aws" {
     region = "us-east-2"
 }
 
+
+
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.instance.id]
 
+#To use a reference inside of a string literal, you need to use a new type of expression called an interpolation, which has the following syntax:
+
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
     tags = {
@@ -21,9 +25,15 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }

6fbf7fb896d3930aceca285d9a1e4327cae06f8b
Erno Paju <erno@Ernos-MacBook-Air.local>
2021-04-29 21:21:40 +0300

    lopussa ja ei toimi

--- terraform-up-and-running-2nd-edition/chapter2/web-server-cluster-with-asg/main.tf
+++ terraform-up-and-running-2nd-edition/chapter2/web-server-cluster-with-asg/main.tf
@@ -25,8 +25,9 @@ resource "aws_autoscaling_group" "example" {
   #use data source to pull subnet ID and to tell ASG to deploy resources to that subnet specified by ID
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
-  #target_group_arns = [aws_lb_target_group.asg.arn]
-  #health_check_type = "ELB"
+  #Attach EC2 instances created by ASG to ALBs target group
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -48,7 +49,7 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
-
+#Security group attached to EC2 instances
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
@@ -59,8 +60,104 @@ resource "aws_security_group" "instance" {
   }
 }
 
+#also in first example
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
+}
+
+#LB resource itself
+resource "aws_lb" "example" {
+
+  name               = var.alb_name
+
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+
+  #Tell LB to use Security "alb" group defined in Security group resource
+  security_groups    = [aws_security_group.alb.id]
+}
+
+#LB listener rules that specify where to forward traffic
+#Rule sends requests to any path to target group attached to ASG
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+#AWS Listener config
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+#LB Security group resource to control inbound and outbound traffic
+resource "aws_security_group" "alb" {
+
+  name = var.alb_security_group_name
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Target group for LB to specify where to forward incoming traffic
+resource "aws_lb_target_group" "asg" {
+
+  name = var.alb_name
+
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+output "alb_dns_name" { 
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer" 
 }

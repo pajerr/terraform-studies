ce6e471ade96d881080df1d1f3c53450e559bbfa
Erno <54817547+pajerr@users.noreply.github.com>
2021-05-10 22:27:39 +0300

    lisätty vikan snapshotin skippaaminen rds mysli instanceen

--- terraform-up-and-running-2nd-edition/chapter3/project-layout-example/stage/data-stores/mysql/main.tf
+++ terraform-up-and-running-2nd-edition/chapter3/project-layout-example/stage/data-stores/mysql/main.tf
@@ -15,6 +15,7 @@ resource "aws_db_instance" "example" {
   engine              = "mysql"
   allocated_storage   = 10
   instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
 
   username            = var.db_username
   name                = var.db_name

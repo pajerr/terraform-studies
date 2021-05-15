b3f9c7d3e227af3e8a6962ded55e7407c23f0c9d
Erno <54817547+pajerr@users.noreply.github.com>
2021-05-15 10:22:44 +0300

    moved staging modules input variables to variables.tf file

--- terraform-up-and-running-2nd-edition/chapter4/stage/services/webserver-cluster/main.tf
+++ terraform-up-and-running-2nd-edition/chapter4/stage/services/webserver-cluster/main.tf
@@ -8,10 +8,3 @@ module "webserver_cluster" {
     source = "../../../modules/services/webserver-cluster"
 }
 
-#these are defined as input variables in module
-cluster_name            = "webservers-stage"
-db_remote_state_bucket  = {}
-db_remote_state_key     = {}
-instance_type           = "t2.nano"
-min_size                = 2
-max_size                = 2
---
+++ terraform-up-and-running-2nd-edition/chapter4/stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,35 @@
+#these are defined as input variables in module
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+  default     = "webservers-stage"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+  type        = string
+}
+
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 3
+}
